54c4db39573ccc0c2191d1ffcbd26cf404743b3c
psycho_punch <psychopunch.me@gmail.com>
2021-09-28 13:44:49 +0100

    Created demo Spring project.

---
+++ src/main/java/io/github/psychopunch/demo/H2JsonJpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.psychopunch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2JsonJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2JsonJpaApplication.class, args);
+	}
+
+}
